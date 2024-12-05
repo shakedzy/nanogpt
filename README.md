928c5d0f1a72cbd180a2400409a00d91f29c88f2
Shaked Zychlinski <shakedzy@gmail.com>
2024-12-05 14:42:19 +0200

    2nd experiment

--- README.md
+++ README.md
@@ -1,3 +1,16 @@
 # NanoGPT
 
 My implementation of Andrej Karpathy's [NanoGPT](https://github.com/karpathy/nanoGPT).
+
+## Data
+The `__resources__` folder contains 3 datasets:
+* `tiny_shakespeare.txt`: same one used in the video
+* `gutenberg_shakespeare.txt`: a copy of [The Complete Works of William Shakespeare](https://www.gutenberg.org/ebooks/100) from The Gutenberg Project
+* `gutenberg_shakespeare_st.txt`: same as above, but with a special character (§) added at the beginning of each play
+
+## Experiments
+The notebook file contains several experiments:
+* Training a simple Bigram Language Model (as seen on the beginning of Andrej's video)
+* Training NanoGPT using a simple character-level encoder
+* Training NanoGPT using OpenAI's GPT-4o encoder
+* Training with and without the special § token, representing a beginning of a new play
--- nanogpt/encoder.py
+++ nanogpt/encoder.py
@@ -1,8 +1,27 @@
-class Encoder:
+import tiktoken
+from abc import ABC, abstractmethod
+
+
+class Encoder(ABC):
+    @abstractmethod
+    def encode(self, text: str) -> list[int]:
+        raise NotImplementedError()
+    
+    @abstractmethod
+    def decode(self, encoded_text: list[int]) -> str:
+        raise NotImplementedError()
+    
+    @abstractmethod
+    def __len__(self):
+        raise NotImplementedError()
+    
+
+class CharacterLevelEncoder(Encoder):
     """
     A character level encoder.
     """
     def __init__(self, input_text: str) -> None:
+        super().__init__()
         self._chars: list[str] = sorted(list(set(input_text)))
         self._encoder: dict[str, int] = {c: i for (i, c) in enumerate(self._chars)}
         self._decoder: dict[int, str] = {i: c for (i, c) in enumerate(self._chars)}
@@ -15,8 +34,27 @@ class Encoder:
     
     def __len__(self):
         return len(self._chars)
+
+
+class TiktokenBasedEncoder(Encoder):
+    """
+    A simplified encoder based on OpenAI's `tiktoken` library
+    """
+    def __init__(self, input_text: str) -> None:
+        super().__init__()
+        self.tokenizer = tiktoken.encoding_for_model('gpt-4o')
+        self._all_tokens = list(set(self.tokenizer.encode(input_text)))
+        self._encoder: dict[int, int] = {c: i for (i, c) in enumerate(self._all_tokens)}
+        self._decoder: dict[int, int] = {i: c for (i, c) in enumerate(self._all_tokens)}
+
+    def encode(self, text: str) -> list[int]:
+        tokens = self.tokenizer.encode(text)
+        return [self._encoder[c] for c in tokens]
     
-    def __contains__(self, character: str):
-        if len(character)!= 1:
-            raise ValueError('Character must be a single character.')
-        return character in self._encoder
+    def decode(self, encoded_text: list[int]) -> str:
+        tokens = [self._decoder[i] for i in encoded_text]
+        return self.tokenizer.decode(tokens)
+    
+    def __len__(self):
+        return len(self._all_tokens)
+    
