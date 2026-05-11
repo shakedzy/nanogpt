fix bug in attention computation
Per "Attention is All You Need" eq. (1), the scaling divisor is √H (head_size), not √C

nanogpt/mlx_/gpt.py
@@ -24,17 +24,18 @@ class Head(nn.Module):
         infs = mx.array([[-1e9] * context_length] * context_length)
         self._inf_triu = mx.triu(infs, k=1)
 
-    def __call__(self, x: mx.array) -> mx.array:  
+    def __call__(self, x: mx.array) -> mx.array:
         B, T, C = x.shape  # (T = context_length, C = input_size)
-        k: mx.array = self.key(x)    # (B, T, T'), T' = head_size
-        q: mx.array = self.query(x)  # (B, T, T')
-        v: mx.array = self.value(x)  # (B, T, T')
-        attention = q @ k.transpose(0, 2, 1) / mx.sqrt(mx.array(C))    # (B, T, T)
+        k: mx.array = self.key(x)    # (B, T, H), H = head_size
+        q: mx.array = self.query(x)  # (B, T, H)
+        v: mx.array = self.value(x)  # (B, T, H)
+        h = k.shape[-1]
+        attention = q @ k.transpose(0, 2, 1) / mx.sqrt(mx.array(h))    # (B, T, T)
         attention = mx.tril(attention, k=0)
         attention += self._inf_triu[:T, :T]
         attention = mx.softmax(attention, axis=-1)
         attention = self.dropout(attention)  # regularization
-        return attention @ v   # (B, T, C)
+        return attention @ v   # (B, T, H)
 
 
 class MultiHeadAttention(nn.Module):

nanogpt/mlx_/multi_stream_gpt.py
@@ -26,18 +26,19 @@ class Head(nn.Module):
         infs = mx.array([[-1e9] * context_length] * context_length)
         self._inf_triu = mx.triu(infs, k=1, stream=stream)
 
-    def __call__(self, x: mx.array) -> mx.array:  
+    def __call__(self, x: mx.array) -> mx.array:
         B, T, C = x.shape  # (T = context_length, C = input_size)
-        k: mx.array = self.key(x)    # (B, T, T'), T' = head_size
-        q: mx.array = self.query(x)  # (B, T, T')
-        v: mx.array = self.value(x)  # (B, T, T')
+        k: mx.array = self.key(x)    # (B, T, H), H = head_size
+        q: mx.array = self.query(x)  # (B, T, H)
+        v: mx.array = self.value(x)  # (B, T, H)
         kT = k.transpose(0, 2, 1, stream=self._stream)
-        attention = mx.matmul(q, kT, stream=self._stream) / mx.sqrt(mx.array(C), stream=self._stream)    # (B, T, T)
+        h = k.shape[-1]
+        attention = mx.matmul(q, kT, stream=self._stream) / mx.sqrt(mx.array(h), stream=self._stream)    # (B, T, T)
         attention = mx.tril(attention, k=0, stream=self._stream)
         attention = mx.add(attention, self._inf_triu[:T, :T], stream=self._stream)
         attention = mx.softmax(attention, axis=-1, stream=self._stream)
         attention = self.dropout(attention)  # regularization
-        return mx.matmul(attention, v, stream=self._stream)   # (B, T, C)
+        return mx.matmul(attention, v, stream=self._stream)   # (B, T, H)
 
 
 class MultiHeadAttention(nn.Module):

nanogpt/torch_/gpt.py
@@ -19,17 +19,18 @@ class Head(nn.Module):
         self.dropout = nn.Dropout(dropout)
         self.register_buffer('tril', torch.tril(torch.ones(context_length, context_length)))
 
-    def forward(self, x: torch.Tensor) -> torch.Tensor:  
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
         B, T, C = x.shape  # (T = context_length, C = input_size)
-        k: torch.Tensor = self.key(x)    # (B, T, T'), T' = head_size
-        q: torch.Tensor = self.query(x)  # (B, T, T')
-        v: torch.Tensor = self.value(x)  # (B, T, T')
+        k: torch.Tensor = self.key(x)    # (B, T, H), H = head_size
+        q: torch.Tensor = self.query(x)  # (B, T, H)
+        v: torch.Tensor = self.value(x)  # (B, T, H)
         # The lines below are the famous attention equation, along with masking and regularization.
-        attention = q @ k.transpose(-2, -1) / torch.sqrt(torch.tensor(C))    # (B, T, T)
+        h = k.size(-1)
+        attention = q @ k.transpose(-2, -1) / torch.sqrt(torch.tensor(h, dtype=q.dtype, device=q.device))    # (B, T, T)
         attention = attention.masked_fill(self.tril[:T,:T] == 0, -float('inf'))  # not allowing future information to flow backwards
-        attention = F.softmax(attention, dim=-1)   
+        attention = F.softmax(attention, dim=-1)
         attention = self.dropout(attention)  # regularization
-        return attention @ v   # (B, T, C)
+        return attention @ v   # (B, T, H)
 
 
 class MultiHeadAttention(nn.Module):